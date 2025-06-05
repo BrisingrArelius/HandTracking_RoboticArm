b76110143979fdef7a2f0ec505a194888ca04a84
BrisingrArelius <tss.tejas@yahoo.com>
2025-06-05 12:49:30 +0530

    Initial test code

---
+++ test.py
@@ -0,0 +1,29 @@
+import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
+hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.5)
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+        break
+
+    image = cv2.flip(image, 1)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image_rgb)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+    cv2.imshow('MediaPipe Hands', image)
+    if cv2.waitKey(5) & 0xFF == 27:  # ESC to quit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
