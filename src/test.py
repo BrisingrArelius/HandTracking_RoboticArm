cae18c0a1633850cf623889d2829ed09de0d302a
BrisingrArelius <tss.tejas@yahoo.com>
2025-07-16 15:57:31 +0530

    Stop tracking some files

--- src/test.py
+++
@@ -1,8 +0,0 @@
-# python - <<'PY'
-from torchvision import datasets, transforms
-root = 'ignore/mini_val'
-try:
-    ds = datasets.ImageFolder(root, transform=transforms.ToTensor())
-    print(f"Found {len(ds)} images, {len(ds.classes)} classes:", ds.classes)
-except Exception as e:
-    print("ImageFolder error:", e)
